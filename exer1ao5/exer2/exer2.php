963f6f68c5305652bc127bb065b1d9021e5b2e22
Rafael <rafaeldovale666@gmail.com>
2022-02-10 19:30:52 -0300

    Organizando pastas e resolvendo o ex2

---
+++ exer1ao5/exer2/exer2.php
@@ -0,0 +1,10 @@
+<?php
+/*
+2) Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boasvindas para ela:
+Ex:
+Qual é o seu nome? João da Silva
+Olá João da Silva, é um prazer te conhecer!
+*/
+$nome = $_POST['nome'];
+
+echo "Olá $nome, é um prazer em te conhecer!";
--- exer1ao5/exer_1.php
+++ exer1ao5/exer_1.php
@@ -7,9 +7,4 @@ echo "<br>";
 $frase = "Olá, Mundo!";
 echo $frase;
 
-/*
-2) Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boasvindas para ela:
-Ex:
-Qual é o seu nome? João da Silva
-Olá João da Silva, é um prazer te conhecer!
-*/
+echo "<br><hr><br>";
