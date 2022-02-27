817036fa5c6c4a2fb522ff7d962e24445713509f
Rafael <rafaeldovale666@gmail.com>
2022-02-27 14:58:02 -0300

    resolvido ex16

--- exer11ao16/exer16/exer16.php
+++ exer11ao16/exer16/exer16.php
@@ -2,13 +2,15 @@
 /*16) [DESAFIO] Escreva um programa para calcular a redução do tempo de vida de um
 fumante. Pergunte a quantidade de cigarros fumados por dias e quantos anos ele
 já fumou. Considere que um fumante perde 10 min de vida a cada cigarro. Calcule
-quantos dias de vida um fumante perderá e exiba o total em dias.
+quantos dias de vida um fumante perderá e exiba o total em dias.*/
 
 $cigarro = $_POST['cigarro'];
 $anos = $_POST['anos'];
 
-$dia  = 1440;// 24horas equivalente em minutos
-$cigarroFumado = $cigarro * 10; //quantidade de cigarro x minuto
-$perca = $dia/$cigarroFumado;
-$percaVida = $anos * $perca;
-echo "$perca e $percaVida";*/
+$fumouNaVida = ($cigarro * 365) * $anos;
+$minutodeFumo = (($cigarro * 365 )* $anos)*10;
+$diasPerdido = $minutodeFumo/ 1440;
+
+echo "Você fumou $fumouNaVida cigarros na sua vida.<br>
+        Você chegou a fumar em minutos com os cáculos $minutodeFumo<br>
+        Então com isso você perdeu $diasPerdido dias    da sua vida";
