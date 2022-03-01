1b1da9c2d6d20fd13f0310bede065af6c4ef4163
Rafael <rafaeldovale666@gmail.com>
2022-03-01 15:51:49 -0300

    Resolvido exer23 e add o indice ao readme

--- Readme.txt
+++ Readme.txt
@@ -108,3 +108,10 @@ situação em relação ao alistamento militar.
 alistamento.
  - Se já tiver depois dos 18 anos, mostre quantos anos já se passaram do
 alistamento.
+
+23) Numa promoção exclusiva para o Dia da Mulher, uma loja quer dar descontos
+para todos, mas especialmente para mulheres. Faça um programa que leia nome,
+sexo e o valor das compras do cliente e calcule o preço com desconto. Sabendo
+que:
+ - Homens ganham 5% de desconto
+ - Mulheres ganham 13% de desconto
---
+++ ex 17 ao 25/exer23/exer23.php
@@ -0,0 +1,22 @@
+<?php
+/*23) Numa promoção exclusiva para o Dia da Mulher, uma loja quer dar descontos
+para todos, mas especialmente para mulheres. Faça um programa que leia nome,
+sexo e o valor das compras do cliente e calcule o preço com desconto. Sabendo
+que:
+ - Homens ganham 5% de desconto
+ - Mulheres ganham 13% de desconto*/
+
+ $nome = $_POST['nome'];
+ $genero = $_POST['sex'];
+ $compra = $_POST['compra'];
+
+ if($_POST['sex'] == 'Feminino')
+ {
+    $DesMulher = $compra - ($compra * 0.15) ;
+    echo "Olá $nome seu genero sendo $genero sua compra teve 15% de desconto e ficou R$$DesMulher";
+ }
+if($_POST['sex'] == 'Masculino')
+{
+    $DesHomem = $compra - ($compra * 0.05);
+    echo "Ola $nome seu genero sendo $genero sua compra teve 5% de desconto e ficou R$$DesHomem";
+}
